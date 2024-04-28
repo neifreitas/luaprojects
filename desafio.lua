61d5ece67a5760578fcd7faf2d7b455848a5abb7
Nei Freitas <122657509+neifreitas@users.noreply.github.com>
2024-04-28 10:55:03 -0300

    Add files via upload

--- desafio.lua
+++ desafio.lua
@@ -15,38 +15,62 @@ local lifeAttr = 5
 local speedAttr = 7
 local inteligenceAttr = 2
 
-local function getProgressBar(attr)
-  local fullChar = "▮"
-  local emptyChar = "▯"
-  
+local function getCardBorder(tam)
+    local barra = "="
     local result = ""
 
-  for i = 1, 10, 1 do
-    if i <= attr then
-       result = result .. fullChar 
-    else
-        result = result .. emptyChar
+    for i = 1, tam, 1 do
+        result = result .. barra
     end
-  end
+    return result
+end
 
+local function getCardSeparator(tam)
+    local barra = "-"
+    local result = ""
+
+    for i = 1, tam, 1 do
+        result = result .. barra
+    end
+    return result
+end
+
+local function getCardTitle(name)
+    local titulo = "|                             " .. "MINECRAFT CARD: " .. name .. "                          |"
+    return titulo
+end
+
+local function getProgressBar(attr)
+    local fullChar = "▮"
+    local emptyChar = "▯"
+    local result = ""
+
+    for i = 1, 10, 1 do
+        if i <= attr then
+            result = result .. fullChar 
+        else
+            result = result .. emptyChar
+        end
+    end
   return result
 end
 
-
 -- Cartão
-print("MINECRAFT CARD ========================================================")
+print(getCardBorder(80))
+print(getCardTitle(monsterName))
+print(getCardBorder(80))
 print("| Nome              : " .. monsterName)
 print("| Descrição         : " .. description)
 print("| Emoji favorito    : " .. emoji)
 print("| Som               : " .. sound)
 print("| Horario favorita  : " .. favoriteTime)
 print("| Item              : " .. item)
-print("| ")
+print("| " .. getCardSeparator(78))
 print("| Atributos")
 print("|    > Ataque       : " .. getProgressBar(attackAttr))
 print("|    > Defesa       : " .. getProgressBar(defenseAttr))
 print("|    > Vida         : " .. getProgressBar(lifeAttr))
 print("|    > Velocidade   : " .. getProgressBar(speedAttr))
 print("|    > Inteligência : " .. getProgressBar(inteligenceAttr))
-print("=======================================================================")
+print(getCardBorder(80))
 
